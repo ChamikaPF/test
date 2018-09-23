919be3a477cf3edf4dbbd7b5ff735e4e2bf1a024
ChamikaPF <40482275+ChamikaPF@users.noreply.github.com>
2018-09-23 17:15:06 +0530

    please enter

---
+++ chami3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Numeric arrays (method 1)</h1>
+	<?php
+	$today=array("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
+	echo "<h3>Today is $today[2]</h2>";
+	?>
+</body>
+</html>
